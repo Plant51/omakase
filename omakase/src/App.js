3b2adae73859a5e9e090866f0939ddf19c1fd1c8
Zach Puller <zach.puller@gmail.com>
2023-07-18 22:49:20 -0700

    connect front end to back end

--- omakase/src/App.js
+++ omakase/src/App.js
@@ -1,13 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useEffect, useState } from 'react'
+
 function App() {
+  const [welcomeMessage, setWelcomeMessage] = useState('')
+  const foo = async () => {
+    const message = await fetch('/api').then(res => res.text())
+    setWelcomeMessage(message)
+  }
+  useEffect(() => {
+    foo()
+  }, [])
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <p>{welcomeMessage}</p>
         </p>
         <a
           className="App-link"
