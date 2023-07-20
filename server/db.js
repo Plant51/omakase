d197a44ed441766a1ad4b0eb0395b7fc17d0b49a
ivyhu <ajianouc@gmail.com>
2023-07-19 21:03:02 -0700

    file structure change

---
+++ server/db.js
@@ -0,0 +1,17 @@
+// db.js
+const mongoose = require('mongoose');
+
+const MONGODB_URI = process.env.MONGODB_URI;
+console.log("MONGODB_URI available:", !!MONGODB_URI, "NODE_ENV=", process.env.NODE_ENV)
+
+mongoose.connect(MONGODB_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+db.once('open', () => {
+  console.log('Connected to MongoDB Atlas database.');
+});
