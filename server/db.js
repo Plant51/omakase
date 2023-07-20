ec140bda433fdd387479be0176efb5d0b44085be
ivyhu <ajianouc@gmail.com>
2023-07-19 23:01:57 -0700

    fix env importing error

--- server/db.js
+++ server/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 const MONGODB_URI = process.env.MONGODB_URI;
-console.log("MONGODB_URI available:", !!MONGODB_URI, "NODE_ENV=", process.env.NODE_ENV)
+console.log("MONGODB_URI available:", !!MONGODB_URI)
 
 mongoose.connect(MONGODB_URI, {
   useNewUrlParser: true,
--- server/server.js
+++ server/server.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
+
 const express = require('express')
 const cors = require('cors');
 const db = require('./db')
 const app = express()
-const port = process.env.PORT || 8080
+
+const port = process.env.BACKEND_PORT || 8080
+
 
 // Enable CORS for all routes
 app.use(cors());
